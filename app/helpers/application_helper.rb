6a27dde77befa0ac894e01cbfecbc628d76a0794
ahmad <mahmadk2@hotmail.com>
2020-07-29 16:31:13 +0500

    add user FUNCTION

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
 
-	
+def has_gravatar(user, options = { size: 80})
+
+gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+
+size = options[:size]
+
+gravatar_url = "http://1.gravatar.com/avatar/6ca195bd70574ed81950d61f1f5af501/#{gravatar_id}?s=#{size}"
+
+image_tag(gravatar_url, alt: user.username, class: "img-circle")
+
+end
 end
