283cbee3285dfc636a041e5bffaa5e044fc26955
ahmad <mahmadk2@hotmail.com>
2020-07-30 18:41:02 +0500

    add icon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,5 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 import 'bootstrap';
 import '../stylesheets/application';
+import "./custom";
+import "@fortawesome/fontawesome-free/js/all";
